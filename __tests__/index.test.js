60c4c0c56c7e5a7ae9cf58235a3a9456f92caba1
thephilgray <thephilgray@gmail.com>
2019-01-26 10:17:52 -0500

    write second test for getFilePaths, testing absolute and realitve input

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,26 @@
 const path = require('path');
-
 const getFilePaths = require('../lib/getFilePaths.js');
 
-const srcPath = path.join(__dirname, 'fixtures');
-
 // 1.
-// accepts a directory and an extension name and returns a list of file paths
-
 describe('getFilePaths', () => {
+  // accepts a directory and an extension name and returns a list of file paths
   it('accepts a directory and an extension name and returns a list of file paths', async () => {
+    const srcPath = path.join(__dirname, 'fixtures');
     const expected = ['page001.html', 'page002.html'];
     const actual = await getFilePaths(srcPath, '.html');
     expect(expected).toEqual(actual);
   });
+  // the directory name can be relative or absolute
+  it('accepts a relative or absolute path to the directory', async () => {
+    const srcPath = '__tests__/fixtures';
+    const expected = ['page001.html', 'page002.html'];
+    const actual = await getFilePaths(srcPath, '.html');
+    expect(expected).toEqual(actual);
+  });
+  // the extension name can contain a '.' or not
+  // can also accept an array of extension names
+  // opts: can return a list of relative file paths or absolute, or just the filenames
 });
-
-// the directory name can be relative or absolute
-// the extension name can contain a '.' or not
-// can also accept an array of extension names
-// opts: can return a list of relative file paths or absolute, or just the filenames
 
 // 2.
 // accepts a filename/filepath and an array of element tagnames
