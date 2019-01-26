413c640322af22ac9b15ec6b7b886bf546df90ed
thephilgray <thephilgray@gmail.com>
2019-01-26 14:39:48 -0500

    ensure selector can be used to return multiple els, and refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,19 +2,21 @@ const path = require('path');
 const getFilePaths = require('../lib/getFilePaths.js');
 const getElementsFromFile = require('../lib/getElementsFromFile.js');
 
+const testHTMLFilenames = ['page001.html', 'page002.html', 'page003.html'];
+
 // 1.
 describe('getFilePaths', () => {
   // accepts a directory and an extension name and returns a list of file paths
   it('accepts a directory and an extension name and returns a list of file paths', async () => {
     const srcPath = path.join(__dirname, 'fixtures');
-    const expected = ['page001.html', 'page002.html'];
+    const expected = testHTMLFilenames;
     const actual = await getFilePaths(srcPath, '.html');
     expect(expected).toEqual(actual);
   });
   // the directory name can be relative or absolute
   it('accepts a relative or absolute path to the directory', async () => {
     const srcPath = '__tests__/fixtures';
-    const expected = ['page001.html', 'page002.html'];
+    const expected = testHTMLFilenames;
     const actual = await getFilePaths(srcPath, '.html');
     expect(expected).toEqual(actual);
   });
@@ -22,7 +24,7 @@ describe('getFilePaths', () => {
   it('accepts the extension name argument with or without "."', async () => {
     const srcPath = path.join(__dirname, 'fixtures');
     const extensionName = 'html';
-    const expected = ['page001.html', 'page002.html'];
+    const expected = testHTMLFilenames;
     const actual = await getFilePaths(srcPath, extensionName);
     expect(expected).toEqual(actual);
   });
@@ -30,7 +32,7 @@ describe('getFilePaths', () => {
   it('accepts the extension name argument as an array of extension names', async () => {
     const srcPath = path.join(__dirname, 'fixtures');
     const extensionName = ['html', '.css'];
-    const expected = ['page001.html', 'page002.html', 'style.css'];
+    const expected = testHTMLFilenames.concat('style.css');
     const actual = await getFilePaths(srcPath, extensionName);
     expect(expected).toEqual(actual);
   });
@@ -41,13 +43,13 @@ describe('getFilePaths', () => {
 
 // 2.
 describe('getElementsFromFile', () => {
+  const tagnames = ['h1', 'h2', 'h3', 'h4', 'h5'];
   const getInstanceNames = HTMLElementsArray =>
     HTMLElementsArray.map(el => el.constructor.name);
 
   // accepts a filename/filepath to an html file and an array of element tagnames
   it('accepts a filename/filepath to an html file and an array of element tagnames and returns an array of elements', async () => {
-    const htmlFile = path.join(__dirname, 'fixtures', 'page002.html');
-    const tagnames = ['h1', 'h2', 'h3'];
+    const htmlFile = path.join(__dirname, 'fixtures', testHTMLFilenames[1]);
     const actual = await getElementsFromFile(htmlFile, tagnames);
 
     const expected = ['HTMLHeadingElement'];
@@ -55,9 +57,8 @@ describe('getElementsFromFile', () => {
     expect(getInstanceNames(actual)).toEqual(expect.arrayContaining(expected));
   });
 
-  it('returns array of elements for all the targetd tags in the document', async () => {
-    const htmlFile = path.join(__dirname, 'fixtures', 'page002.html');
-    const tagnames = ['h1', 'h2', 'h3', 'h4', 'h5'];
+  it('returns array of elements for all the targeted tags in the document', async () => {
+    const htmlFile = path.join(__dirname, 'fixtures', testHTMLFilenames[1]);
     const actual = await getElementsFromFile(htmlFile, tagnames);
 
     const expected = [
@@ -67,6 +68,15 @@ describe('getElementsFromFile', () => {
     ];
     // should be an array of html elements
     expect(getInstanceNames(actual)).toEqual(expected);
+  });
+
+  it('returns mutliple elements from the same selector', async () => {
+    const htmlFile = path.join(__dirname, 'fixtures', testHTMLFilenames[2]);
+    const actual = await getElementsFromFile(htmlFile, tagnames);
+
+    // should be an array of html elements
+    expect(actual[2].tagName).toEqual('H3');
+    expect(actual[3].tagName).toEqual('H3');
   });
 });
 
